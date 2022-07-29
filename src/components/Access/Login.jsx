05b97e64abc5bd904a4451d79e369c16affc5722
Herman Muhereza <hermanmuhereza22@gmail.com>
2022-07-29 15:44:49 +0300

    setting app structure

---
+++ src/components/Access/Login.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Login() {
+  return (
+    <div className='Login-form'>
+        <p>To display login form</p>
+    </div>
+  )
+}
---
+++ src/components/Api/Api.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Api() {
+  return (
+    <div>
+        <p>to handle connection to rest api</p>
+    </div>
+  )
+}
