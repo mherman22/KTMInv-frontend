7d456370852508c28a78be7cf86d85d715943090
mherman22 <hermanmuhereza22@gmail.com>
2022-08-05 10:33:34 +0300

    improve on the css for the login and registration

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
 import FormInput from '../Form Input/FormInput';
-import './Login.css';
+import '../index.css';
 
 export default function Login() {
 
   const [values, setValues] = useState({
-    username:"",password:""
+    username: "", password: ""
   });
 
   const handleSubmit = e => {
@@ -14,37 +14,39 @@ export default function Login() {
   }
 
   const handleValueChange = e => {
-    setValues({...values,[e.target.name]: e.target.value});
+    setValues({ ...values, [e.target.name]: e.target.value });
   }
 
   const LoginInputs = [
-    {id:1, name:"username", type:"text", placeholder:"userName",
-        error:"Username is required!",
-        label:"username",
-        required:true
+    {
+      id: 1, name: "username", type: "text", placeholder: "userName",
+      error: "Username is required!",
+      label: "username",
+      required: true
     },
-    {id:2, name:"password", type:"password", placeholder:"Password",
-        error:"Password is required!",
-        label:"Password",
-        required:true
+    {
+      id: 2, name: "password", type: "password", placeholder: "Password",
+      error: "Password is required!",
+      label: "Password",
+      required: true
     },
-]
+  ]
 
   return (
     <div className='Login-form'>
-        <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit}>
         <h1>Login</h1>
-          {LoginInputs.map(input =>(
-            <FormInput 
-            key={input.id} 
-            {...input} 
-            value={values[input.name]} 
+        {LoginInputs.map(input => (
+          <FormInput
+            key={input.id}
+            {...input}
+            value={values[input.name]}
             onChange={handleValueChange}
-            />
-          ))}
-          <button>Login</button>
-          <p>New User?  <a href='#'> Register</a></p>
-        </form>
+          />
+        ))}
+        <button>Login</button>
+        <p>New User?  <a href='/src/components/Register/Register.jsx'> Register</a></p>
+      </form>
     </div>
   )
 }
